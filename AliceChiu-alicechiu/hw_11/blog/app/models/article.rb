e2a1084a7350751395bbe326807efbd9cfb02a8e
Alice Chiu <alicejchiu@gmail.com>
2015-09-19 10:40:19 -0700

    added validates timeliness gem

--- AliceChiu-alicechiu/hw_11/blog/app/models/article.rb
+++ AliceChiu-alicechiu/hw_11/blog/app/models/article.rb
@@ -1,6 +1,7 @@
 class Article < ActiveRecord::Base
   validates :title, presence: true,
-                    length: { minimum: 5}
-  validates :date, presence:true,
-                    date: {on_or_after: 1920}
+                    length: {minimum: 5}
+
+  validates_datetime :date_start, :on_or_before => lambda { Date.current }
+
 end
--- AliceChiu-alicechiu/hw_11/blog/db/migrate/20150913003323_create_articles.rb
+++ AliceChiu-alicechiu/hw_11/blog/db/migrate/20150913003323_create_articles.rb
@@ -2,7 +2,7 @@ class CreateArticles < ActiveRecord::Migration
   def change
     create_table :articles do |t|
       t.string :title
-      t.string :date
+      t.datetime :date
       t.text :body
       t.string :author
       t.boolean :private
